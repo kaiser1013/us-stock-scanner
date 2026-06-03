76b6b953d741a1ae41f0fb715b7897013c688143
kaiser1013 <kaiser1013@gmail.com>
2026-06-03 15:38:55 -0400

    Update stock_scan.py

--- stock_scan.py
+++ stock_scan.py
@@ -1,1 +1,27 @@
-print("Scanner started")
+import os
+import smtplib
+from email.message import EmailMessage
+
+msg = EmailMessage()
+
+msg["Subject"] = "US Stock Scanner Test"
+msg["From"] = os.getenv("EMAIL_USER")
+msg["To"] = os.getenv("EMAIL_TO")
+
+msg.set_content("Hello from GitHub Actions!")
+
+server = smtplib.SMTP_SSL(
+    "smtp.gmail.com",
+    465
+)
+
+server.login(
+    os.getenv("EMAIL_USER"),
+    os.getenv("EMAIL_PASSWORD")
+)
+
+server.send_message(msg)
+
+server.quit()
+
+print("Email sent successfully")
